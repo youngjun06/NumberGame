87561e76e32c23067320d5fdeaf97c278382dbd3
Na young jun <nyj061025@gmail.com>
2025-08-13 23:55:07 +0900

    Update main.py

--- BreakOut/main.py
+++ BreakOut/main.py
@@ -15,65 +15,84 @@ start_font = pygame.font.SysFont(None, 100)
 # Button
 start_button = pygame.Rect(width // 2 - 80, height // 2, 160, 50)
 recode_button = pygame.Rect(width // 2 - 80, height // 2 + 80, 160, 50)
-exit_button = pygame.Rect(width // 2 -80, height // 2 + 160, 160, 50)
+exit_button = pygame.Rect(width // 2 - 80, height // 2 + 160, 160, 50)
+back_button = pygame.Rect(width // 2 - 80, height // 2 + 160, 160, 50)
 
-# Game loop
+screen_state = "start"
 running = True
-game_start = False
-recode_look = False
-while running:
+
+def StartScreen():
+    screen.fill((250, 250, 250))
+    # Title
+    title_text = title_font.render("<Break Out>", True, (0, 0, 0))
+    title_rect = title_text.get_rect(center=(width // 2, height // 2 - 180))
+    screen.blit(title_text, title_rect)
+
+    # Start button
+    pygame.draw.ellipse(screen, (255, 0, 0), start_button)
+    start_button_text = any_font.render("Start", True, (255, 255, 255))
+    start_button_rect = start_button_text.get_rect(center=(start_button.center))
+    screen.blit(start_button_text, start_button_rect)
+
+    # Recode button
+    pygame.draw.ellipse(screen, (100, 100, 100), recode_button)
+    recode_button_text = any_font.render("Recode", True, (255, 255, 255))
+    recode_button_rect = recode_button_text.get_rect(center=(recode_button.center))
+    screen.blit(recode_button_text, recode_button_rect)
+
+    # Exit button
+    pygame.draw.ellipse(screen, (50, 50, 50), exit_button)
+    exit_button_text = any_font.render("Exit", True, (255, 255, 255))
+    exit_button_rect = exit_button_text.get_rect(center=(exit_button.center))
+    screen.blit(exit_button_text, exit_button_rect)
+
+def GameScreen():
+    screen.fill((250, 250, 250))
+    game_text = start_font.render("START", True, (150, 150, 150))
+    game_rect = game_text.get_rect(center=(width // 2, height // 2 - 70))
+    screen.blit(game_text, game_rect)
+
+def RecodeScreen():
+    screen.fill((250, 250, 250))
+    recode_text = title_font.render("Recode", True, (0, 0, 0))
+    recode_rect = recode_text.get_rect(center=(width // 2, 80))
+    screen.blit(recode_text, recode_rect)
+    
+    #Back Button
+    pygame.draw.ellipse(screen, (50, 50, 50), back_button)
+    exit_button_text = any_font.render("Back", True, (255, 255, 255))
+    exit_button_rect = exit_button_text.get_rect(center=(exit_button.center))
+    screen.blit(exit_button_text, exit_button_rect)
+
+
+def ControlGame():
+    global screen_state, running
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if exit_button.collidepoint(event.pos):
-                running = False
-            if not game_start and start_button.collidepoint(event.pos):
-                game_start  = True
-            if not recode_look and recode_button.collidepoint(event.pos):
-                recode_look = True
-    
-    # start
-    screen.fill((250, 250, 250))
+            if screen_state == "start":
+                if start_button.collidepoint(event.pos):
+                    screen_state = "game"
+                elif recode_button.collidepoint(event.pos):
+                    screen_state = "recode"
+                elif exit_button.collidepoint(event.pos):
+                    running = False
+            elif screen_state == "recode":
+                if back_button.collidepoint(event.pos):
+                    screen_state = "start"
+            elif screen_state == "game":
+                if exit_button.collidepoint(event.pos):
+                    running = False
 
-    # Click recode button
-    if recode_look: 
-        recode_text = title_font.render("Recode", True, (0, 0, 0))
-        recode_rect = recode_text.get_rect(center=(width // 2, 50))
-        screen.blit(recode_text, recode_rect)
-    
-    elif not game_start:
-        # Title
-        title_text = title_font.render("<Break Out>", True, (0, 0, 0))
-        title_rect = title_text.get_rect(center=(width // 2, height // 2 - 180))
-        screen.blit(title_text, title_rect)
-
-        # Start button
-        pygame.draw.ellipse(screen, (255, 0, 0), start_button)
-        start_button_text = any_font.render("Start", True, (255, 255, 255))
-        start_button_rect = start_button_text.get_rect(center=(start_button.center))
-        screen.blit(start_button_text, start_button_rect)
-
-        # Recode button
-        pygame.draw.ellipse(screen, (100, 100, 100), recode_button)
-        recode_button_text = any_font.render("Recode", True, (255, 255, 255))
-        recode_button_rect = recode_button_text.get_rect(center=(recode_button.center))
-        screen.blit(recode_button_text, recode_button_rect)
-
-        # Exit button
-        pygame.draw.ellipse(screen, (50, 50, 50), exit_button)
-        exit_button_text = any_font.render("Exit", True, (255, 255, 255))
-        exit_button_rect = exit_button_text.get_rect(center=(exit_button.center))
-        screen.blit(exit_button_text, exit_button_rect)
-
-    # Click start button
-    elif game_start: 
-        game_text = start_font.render("START", True, (150, 150, 150))
-        game_rect = game_text.get_rect(center=(width // 2, height // 2 - 70))
-        screen.blit(game_text, game_rect)
-
-
-
-
+# Game loop
+while running:
+    ControlGame()
+    if screen_state == "start":
+        StartScreen()
+    elif screen_state == "game":
+        GameScreen()
+    elif screen_state == "recode":
+        RecodeScreen()
     pygame.display.flip()
     clock.tick(60)
